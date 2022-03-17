cd8b1c167cdf2da7fb55d03df03b26dd644adec5
lyamz <davidlyangenda@gmail.com>
2022-03-17 12:24:11 +0200

    Update HTML & JS files

--- main.js
+++ main.js
@@ -21,20 +21,26 @@ class MainUI {
   };
 
   createBook(book) {
+    const booksContainer = document.querySelector('.books-container');
     const bookContainer = document.createElement('div');
-    bookContainer.className = 'single-book';
-    const bookTitle = document.createElement('h3');
-    bookTitle.className = 'title';
-    const bookAuthor = document.createElement('h3');
-    bookAuthor.className = 'author';
-    const removeButton = document.createElement('button');
-    removeButton.className = 'remove-button';
-    removeButton.textContent = 'Remove';
-    bookTitle.textContent = book.title;
-    bookAuthor.textContent = book.author;
-    bookContainer.append(bookTitle, bookAuthor, removeButton);
+bookContainer.className = 'single-book';
+const authorContainer = document.createElement('div');
+authorContainer.className='author-name'
+const span =document.createElement('span');
+span.textContent= 'by';
+const bookTitle = document.createElement('h3');
+bookTitle.className = 'title';
+const bookAuthor = document.createElement('h3');
+bookAuthor.className = 'author';
+const removeButton = document.createElement('button');
+removeButton.className = 'remove-button';
+removeButton.textContent = 'Remove';
+bookTitle.textContent = book.title;
+bookAuthor.textContent = book.author;
+authorContainer.append(bookTitle, span, bookAuthor);
+bookContainer.append(authorContainer, removeButton); ;
   
-    main.insertBefore(bookContainer, form);
+booksContainer.appendChild(bookContainer)
   };
 
   addBook(book) {
