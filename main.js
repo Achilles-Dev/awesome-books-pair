5391cc397cd0a0fea05507b8e0cc6689628a0362
Solomon Achilles <solohag92@gmail.com>
2022-03-17 11:18:22 +0000

    Fix linter errors

--- main.js
+++ main.js
@@ -1,87 +1,34 @@
+/* eslint-disable max-classes-per-file */
+
 const form = document.querySelector('.input-form');
-const main = document.querySelector('.main');
 
 class Book {
   constructor(title, author) {
-    this.title = title,
-    this.author = author
-  };
-};
-
-class MainUI {
-
-  populateBooks() {
-    const store = new Store();
-    const books = store.getBooks();
-      books.forEach((book) => {
-        const mainUi = new MainUI();
-        mainUi.createBook(book);
-      });
-    
-  };
-
-  createBook(book) {
-    const booksContainer = document.querySelector('.books-container');
-    const bookContainer = document.createElement('div');
-bookContainer.className = 'single-book';
-const authorContainer = document.createElement('div');
-authorContainer.className='author-name'
-const span =document.createElement('span');
-span.textContent= 'by';
-const bookTitle = document.createElement('h3');
-bookTitle.className = 'title';
-const bookAuthor = document.createElement('h3');
-bookAuthor.className = 'author';
-const removeButton = document.createElement('button');
-removeButton.className = 'remove-button';
-removeButton.textContent = 'Remove';
-bookTitle.textContent = book.title;
-bookAuthor.textContent = book.author;
-authorContainer.append(bookTitle, span, bookAuthor);
-bookContainer.append(authorContainer, removeButton); ;
-  
-booksContainer.appendChild(bookContainer)
-  };
-
-  addBook(book) {
-    const mainUi = new MainUI();
-    mainUi.createBook(book);
-  };
-
-  removeBook(button) {
-    const parentDiv = button.parentNode;
-    parentDiv.remove();
+    this.title = title;
+    this.author = author;
   }
-
-  clearInputs() {
-    form.elements.title.value = '';
-    form.elements.author.value = '';
-  }
-
-};
+}
 
 class Store {
-  getBooks() {
+  getBooks = () => {
     let books;
     if (localStorage.getItem('booksStored') === null) {
       books = [];
-    } else {  
+    } else {
       books = JSON.parse(window.localStorage.getItem('booksStored'));
-    }  
+    }
     return books;
   }
 
-  addBook(book) {
-    const store = new Store();
-    const books = store.getBooks();
+  addBook = (book) => {
+    const books = this.getBooks();
 
     books.push(book);
     window.localStorage.setItem('booksStored', JSON.stringify(books));
   }
 
-  removeBook(button) {
-    const store = new Store();
-    const books = store.getBooks();
+  removeBook = (button) => {
+    const books = this.getBooks();
     const parentDiv = button.parentNode;
     const myTitle = parentDiv.querySelector('.title').textContent;
     const myAuthor = parentDiv.querySelector('.author').textContent;
@@ -91,24 +38,71 @@ class Store {
   }
 }
 
-// Display Books 
+class MainUI {
+  populateBooks = () => {
+    const store = new Store();
+    const books = store.getBooks();
+    books.forEach((book) => {
+      this.createBook(book);
+    });
+  }
+
+  createBook = (book) => {
+    const booksContainer = document.querySelector('.books-container');
+    const bookContainer = document.createElement('div');
+    bookContainer.className = 'single-book';
+    const authorContainer = document.createElement('div');
+    authorContainer.className = 'author-name';
+    const span = document.createElement('span');
+    span.textContent = 'by';
+    const bookTitle = document.createElement('h3');
+    bookTitle.className = 'title';
+    const bookAuthor = document.createElement('h3');
+    bookAuthor.className = 'author';
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
+    removeButton.textContent = 'Remove';
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    authorContainer.append(bookTitle, span, bookAuthor);
+    bookContainer.append(authorContainer, removeButton);
+
+    booksContainer.appendChild(bookContainer);
+  }
+
+  addBook = (book) => {
+    this.createBook(book);
+  }
+
+  removeBook = (button) => {
+    const parentDiv = button.parentNode;
+    parentDiv.remove();
+  }
+
+  clearInputs = () => {
+    form.elements.title.value = '';
+    form.elements.author.value = '';
+  }
+}
+
+// Display Books
 const mainPage = new MainUI();
 const store = new Store();
 document.addEventListener('DOMContentLoaded', mainPage.populateBooks());
 
-// Add Book 
+// Add Book
 const addButton = document.querySelector('#add-button');
 addButton.addEventListener('click', () => {
   const title = form.elements.title.value;
   const author = form.elements.author.value;
   const newBook = new Book(title, author);
-  
+
   mainPage.addBook(newBook);
   store.addBook(newBook);
   mainPage.clearInputs();
 });
 
-//Remove Book
+// Remove Book
 
 document.addEventListener('click', (e) => {
   const button = e.target;
